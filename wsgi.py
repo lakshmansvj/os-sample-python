b5afad62fbafe9fba497cc6d6e94f8282d696a07
lakshmansvj <31065090+lakshmansvj@users.noreply.github.com>
2017-08-28 12:26:45 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World!"
+    return "Hello World!!!  This is a sample proj"
 
 if __name__ == "__main__":
     application.run()
